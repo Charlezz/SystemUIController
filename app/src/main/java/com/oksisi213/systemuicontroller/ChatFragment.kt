6e74a59a5ad2d918da5e9a603dd413832b2c17b8
Charles ok <shok@maxst.com>
2018-01-17 14:24:06 +0900

    enable button

--- app/src/main/java/com/oksisi213/systemuicontroller/ChatFragment.kt
+++ app/src/main/java/com/oksisi213/systemuicontroller/ChatFragment.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_chat.view.*
 
 
 /**
@@ -23,7 +24,7 @@ class ChatFragment : Fragment(), MyWindowFocusChangeListener {
 
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 		val view = inflater.inflate(R.layout.fragment_chat, container, false)
-		view.setOnClickListener {
+		view.btn.setOnClickListener {
 			activity?.startActivity(Intent(activity, ChatActivity::class.java))
 		}
 		return view
